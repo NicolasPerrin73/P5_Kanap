6c27396f39fd673e4e5ecf48296c81826030c7c2
NicolasPerrin73 <nicobolas7338@gmail.com>
2022-06-01 19:59:20 +0200

    build addToCart function

--- front/js/product.js
+++ front/js/product.js
@@ -1,65 +1,66 @@
 // Use searchParams method for getting the id in url
 
-let url = window.location.href
-console.log(url)
+let url = window.location.href;
 
-let u = new URL(url)
+let u = new URL(url);
 
-let id = u.searchParams.get('id')
-console.log(id)
+let id = u.searchParams.get("id");
 
-let itemImg = document.querySelector('.item__img')
-console.log(itemImg)
+let itemImg = document.querySelector(".item__img");
 
-const addProduct = function(product){
-                        
-         // Image product
-         let img = document.createElement('img')
-         img.setAttribute('src', product.imageUrl)
-         img.setAttribute('alt', product.altTxt)
-         itemImg.appendChild(img)
+const addProduct = function (product) {
+  // Image product
+  let img = document.createElement("img");
+  img.setAttribute("src", product.imageUrl);
+  img.setAttribute("alt", product.altTxt);
+  itemImg.appendChild(img);
 
-         // Product title
-         let title = document.querySelector('#title')
-         title.textContent = `${product.name}`
+  // Product title
+  let title = document.querySelector("#title");
+  title.textContent = `${product.name}`;
 
-         //Product price
-         let price = document.querySelector('#price')
-         price.textContent = `${product.price}`
+  //Product price
+  let price = document.querySelector("#price");
+  price.textContent = `${product.price}`;
 
-         //Product description
-         let description = document.querySelector('#description')
-         description.textContent = `${product.description}`
+  //Product description
+  let description = document.querySelector("#description");
+  description.textContent = `${product.description}`;
 
-         // Product color option
-        for(let i = 0; i < product.colors.length; i++){
-            let option = document.createElement('option')
-            option.setAttribute('value', product.colors[i])
-            option.textContent = `${product.colors[i]}`
-            let select = document.querySelector('#colors')
-            select.appendChild(option)
-        }
-
-        
-    }
-    
-
+  // Product color option
+  for (let i = 0; i < product.colors.length; i++) {
+    let option = document.createElement("option");
+    option.setAttribute("value", product.colors[i]);
+    option.textContent = `${product.colors[i]}`;
+    let select = document.querySelector("#colors");
+    select.appendChild(option);
+  }
+};
 
 // API call function
 fetch(`http://localhost:3000/api/products/${id}`)
-    .then(function(res){
-        if(res.ok){
-            return res.json();
-        }
-    })
-    .then(function(product){
-          console.log(product);
-          addProduct(product)
-            }
-        
-        
-    )
-    .catch(function(err){
-        console.log("Erreur" +  err)
-    })
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (product) {
+    addProduct(product);
+  })
+  .catch(function (err) {
+    console.log("Erreur" + err);
+  });
 
+// Add to cart
+const addTocart = function () {
+  document.querySelector("#addToCart").addEventListener("click", function () {
+    let quantityInput = document.querySelector("#quantity").value;
+
+    let colorInput = document.getElementById("colors");
+    let colorSelected = colorInput.options[colorInput.selectedIndex].value;
+
+    console.log(colorSelected, quantityInput, id);
+  });
+};
+
+addTocart();
--- front/js/script.js
+++ front/js/script.js
@@ -1,38 +1,31 @@
-const items = document.getElementById('items')
+const items = document.getElementById("items");
 
+// Function to add item
+const createItem = function (products) {
+  for (let i = 0; i < products.length; i++) {
+    const item = document.createElement("a");
 
-// Function to add item 
-const createItem = function(products){
-    for(let i = 0; i < products.length; i++){
-
-        const item = document.createElement('a')
-
-        item.innerHTML = `
+    item.innerHTML = `
         <article>
         <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
         <h3 class="productName">${products[i].name}</h3>
         <p class="productDescription">${products[i].description}.</p>
-        </article>`
-        item.setAttribute('href', `./product.html?id=${products[i]._id}`)
+        </article>`;
+    item.setAttribute("href", `./product.html?id=${products[i]._id}`);
 
-        items.appendChild(item)
-}
-}
+    items.appendChild(item);
+  }
+};
 // API call function
-fetch('http://localhost:3000/api/products')
-    .then(function(res){
-        if(res.ok){
-            return res.json();
-        }
-    })
-    .then(function(products){
-            createItem(products)           
-        }
-        
-    )
-    .catch(function(err){
-        console.log("Erreur" +  err)
-    })
-
-createItem(products)
-
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (products) {
+    createItem(products);
+  })
+  .catch(function (err) {
+    console.log("Erreur" + err);
+  });
